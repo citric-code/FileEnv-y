4cb4fce9f93bcc493063c5d68d605b10d7a9a3e3
GitHub Action <action@github.com>
2025-06-02 20:54:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/citriccode/fileenvy/services/MyProjectService.kt
+++ src/main/kotlin/com/github/citriccode/fileenvy/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.citriccode.fileenvy.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.citriccode.fileenvy.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
